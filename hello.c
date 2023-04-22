38a80b03132bf87d2ff96f41611ff95917b3bf09
mou7s <liuchuge7777777@gmail.com>
2023-04-22 21:51:39 +0800

    #

--- hello.c
+++ hello.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
-void inplace_swap(int *x, int *y){
-    *y = *x ^ *y;
-    *x = *x ^ *y;
-    *y = *x ^ *y;
-}
-void reverse_array(int arr[], int size) {
-    int i;
-    for (i = 0; i < size/2; i++) {
-        inplace_swap(&arr[i], &arr[size-i-1]);
+
+void func1(void);
+
+static int count = 10;
+
+int main()
+{
+    while (count --)
+    {
+        func1();
     }
-}
-
-int main() {
-    int arr[] = {1, 2, 3, 4, 5};
-    int size = sizeof(arr)/sizeof(arr[0]);
-    int i;
-
-    printf("Before reverse: ");
-    for (i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    reverse_array(arr, size);
-
-    printf("\nAfter reverse: ");
-    for (i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-printf ("\n");
     return 0;
 }
 
+void func1(void)
+{
+
+    static int thingy = 5;
+    thingy++;
+    printf("thingy is %d, count is %d\n", thingy, count);
+}
