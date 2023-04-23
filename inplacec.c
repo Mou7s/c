2fe689031b2c60b46799fbff63234452d456fb5f
Mou7s <79881792+Mou7s@users.noreply.github.com>
2023-04-23 19:50:36 +0800

    #

--- inplacec.c
+++ inplacec.c
@@ -7,11 +7,31 @@ void inplace_swap(int *x, int *y)
     *y = *x ^ *y;
 }
 
+void reverse_array(int a[], int cnt)
+{
+
+    int first, last;
+    for (first = 0, last = cnt - 1; first <= last; first++, last--)
+    {
+        inplace_swap(&a[first], &a[last]);
+    }
+}
+
 int main()
 {
-    int a = 3;
-    int b = 5;
 
-    inplace_swap(&a, &b);
+    int arr[] = {1, 2, 3, 4, 5};
+
+    int cnt = sizeof(arr) / sizeof(int);
+    for (int i = 0; i < cnt; i++)
+    {
+        printf("%d", arr[i]);
+    }
+    printf("\n");
+    reverse_array(arr, cnt);
+    for (int i = 0; i < cnt; i++)
+    {
+        printf("%d", arr[i]);
+    }
     return 0;
 }
