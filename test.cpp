beaacd7fd22fbc75f5f8e1dc124bdd9b27625f92
Mou7s <79881792+Mou7s@users.noreply.github.com>
2023-05-11 11:18:30 +0000

    #

--- test.cpp
+++ test.cpp
@@ -1,12 +1,50 @@
 #include <iostream>
 using namespace std;
+
+class Shape
+{
+public:
+    void setWidth(int w)
+    {
+        width = w;
+    }
+    void setHeight(int h)
+    {
+        height = h;
+    }
+
+protected:
+    int width;
+    int height;
+};
+
+class PaintCost
+{
+public:
+    int getCost(int area)
+    {
+        return area * 70;
+    }
+};
+
+class Rectangle : public Shape, public PaintCost
+{
+public:
+    int getArea()
+    {
+        return (width * height);
+    }
+};
+
 int main()
 {
-    int nCase, nFeet;
-    cin >> nCase;
+    Rectangle Rect;
+    Rect.setWidth(5);
+    Rect.setHeight(7);
+    int area = Rect.getArea();
 
-    for (int i = 0; i < nCase; i++)
-    {
-        cin >> nFeet;
-    }
+    cout << "Total area: " << area << endl;
+    cout << "Total paint cost: $" << Rect.getCost(area) << endl;
+
+    return 0;
 }
