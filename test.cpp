cddbda2901e622e183a8de41159c355824ecb302
Mou7s <liuchuge7777777@gmail.com>
2023-05-20 17:25:49 +0800

    ##

--- test.cpp
+++ test.cpp
@@ -1,44 +1,58 @@
 #include <iostream>
 using namespace std;
 
-bool check(int arr[], int row, int col)
+// 抽象产品类
+class Product
 {
-    for (int i = 0; i < row; i++)
-    {
-        if (arr[i] == col || abs(arr[i] - col) == row - i)
-        {
-            return false;
-        }
-    }
-    return true;
-}
+public:
+    virtual void use() = 0;
+};
 
-void dfs(int arr[], int row, int n, int &count)
+// 具体产品类 A
+class ConcreteProductA : public Product
 {
-    if (row == n)
+public:
+    void use() override
     {
-        count++;
-        return;
+        cout << "Using ConcreteProductA" << endl;
     }
-    for (int i = 0; i < n; i++)
+};
+
+// 具体产品类 B
+class ConcreteProductB : public Product
+{
+public:
+    void use() override
     {
-        if (check(arr, row, i))
+        cout << "Using ConcreteProductB" << endl;
+    }
+};
+
+// 工厂类
+class Factory
+{
+public:
+    static Product *createProduct(int type)
+    {
+        switch (type)
         {
-            arr[row] = i;
-            dfs(arr, row + 1, n, count);
-            arr[row] = 0;
+        case 1:
+            return new ConcreteProductA();
+        case 2:
+            return new ConcreteProductB();
+        default:
+            return nullptr;
         }
     }
-}
+};
 
 int main()
 {
-    int n;
-    cout << "请输入皇后数量：";
-    cin >> n;
-    int arr[n] = {0};
-    int count = 0;
-    dfs(arr, 0, n, count);
-    cout << "共有 " << count << " 种放置方法。" << endl;
+    Product *productA = Factory::createProduct(1);
+    Product *productB = Factory::createProduct(2);
+    productA->use();
+    productB->use();
+    delete productA;
+    delete productB;
     return 0;
 }
