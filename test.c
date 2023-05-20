2acd39db1c60ae821e75ca468e87d2e3730215e6
Mou7s <liuchuge7777777@gmail.com>
2023-05-21 01:07:17 +0800

    #

--- test.c
+++ test.c
@@ -1,42 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int max(int a, int b)
 {
-    return a > b ? a : b;
+    return (a > b) ? a : b;
 }
 
 int knapsack(int W, int wt[], int val[], int n)
 {
-    int dp[n + 1][W + 1];
-    for (int i = 0; i <= n; i++)
+    int i, w;
+    int K[n + 1][W + 1];
+
+    // 初始化数组 K
+    for (i = 0; i <= n; i++)
     {
-        for (int j = 0; j <= W; j++)
+        for (w = 0; w <= W; w++)
         {
-            if (i == 0 || j == 0)
+            if (i == 0 || w == 0)
             {
-                dp[i][j] = 0;
+                K[i][w] = 0;
             }
-            else if (wt[i - 1] > j)
+            else if (wt[i - 1] <= w)
             {
-                dp[i][j] = dp[i - 1][j];
+                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
             }
             else
             {
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - wt[i - 1]] + val[i - 1]);
+                K[i][w] = K[i - 1][w];
             }
         }
     }
-    return dp[n][W];
+
+    return K[n][W];
 }
 
 int main()
 {
-    int W = 50;
-    int wt[] = {10, 20, 30};
     int val[] = {60, 100, 120};
-    int n = sizeof(wt) / sizeof(wt[0]);
-    int max_val = knapsack(W, wt, val, n);
-    printf("The maximum value that can be put in a knapsack of capacity %d is %d\n", W, max_val);
+    int wt[] = {10, 20, 30};
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+    printf("Maximum value: %d", knapsack(W, wt, val, n));
     return 0;
 }
