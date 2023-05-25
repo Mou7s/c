a30bf859949bcbb6359b6b6707a2a0fe7838df74
Mou7s <79881792+Mou7s@users.noreply.github.com>
2023-05-25 11:47:48 +0000

    #

--- test.c
+++ test.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int max(int a, int b)
 {
@@ -9,11 +8,7 @@ int max(int a, int b)
 int knapsack(int W, int wt[], int val[], int n)
 {
     int i, w;
-    int **K = (int **)malloc((n + 1) * sizeof(int *));
-    for (i = 0; i <= n; i++)
-    {
-        K[i] = (int *)malloc((W + 1) * sizeof(int));
-    }
+    int K[n + 1][W + 1];
 
     for (i = 0; i <= n; i++)
     {
@@ -34,15 +29,7 @@ int knapsack(int W, int wt[], int val[], int n)
         }
     }
 
-    int result = K[n][W];
-
-    for (i = 0; i <= n; i++)
-    {
-        free(K[i]);
-    }
-    free(K);
-
-    return result;
+    return K[n][W];
 }
 
 int main()
@@ -50,7 +37,7 @@ int main()
     int val[] = {60, 100, 120};           // value
     int wt[] = {10, 20, 30};              // weight
     int W = 50;                           // capacity
-    int n = sizeof(val) / sizeof(val[0]); // num;
+    int n = sizeof(val) / sizeof(val[0]); // nums
     printf("Maximum value that can be obtained = %d", knapsack(W, wt, val, n));
     return 0;
 }
