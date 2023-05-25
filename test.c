e6e0801d0552d82c4f0220ea8305867762d698af
mou7s <liuchuge7777777@gmail.com>
2023-05-25 18:57:59 +0800

    #

--- test.c
+++ test.c
@@ -1,77 +1,70 @@
 #include <stdio.h>
 
-void merge(int arr[], int l, int m, int r)
+// 定义背包容量和物品数量的常量
+#define MAX_WEIGHT 50
+#define NUM_ITEMS 10
+
+// 定义物品的结构体
+struct Item
 {
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    int L[n1], R[n2];
-    for (i = 0; i < n1; i++)
-    {
-        L[i] = arr[l + i];
-    }
-    for (j = 0; j < n2; j++)
-    {
-        R[j] = arr[m + 1 + j];
-    }
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= R[j])
-        {
-            arr[k] = L[i];
-            i++;
-        }
-        else
-        {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-    while (i < n1)
-    {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-    while (j < n2)
-    {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+    int weight; // 物品重量
+    int value;  // 物品价值
+};
+
+// 计算两个整数中的最大值
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
 }
 
-void mergeSort(int arr[], int l, int r)
+// 计算01背包问题的最大价值
+int knapsack(struct Item items[], int n)
 {
-    if (l < r)
+    int i, w;
+    int K[n + 1][MAX_WEIGHT + 1];
+
+    // 填充K[][]表格
+    for (i = 0; i <= n; i++)
     {
-        int m = l + (r - l) / 2;
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-        merge(arr, l, m, r);
+        for (w = 0; w <= MAX_WEIGHT; w++)
+        {
+            if (i == 0 || w == 0)
+            {
+                K[i][w] = 0;
+            }
+            else if (items[i - 1].weight <= w)
+            {
+                K[i][w] = max(items[i - 1].value + K[i - 1][w - items[i - 1].weight], K[i - 1][w]);
+            }
+            else
+            {
+                K[i][w] = K[i - 1][w];
+            }
+        }
     }
+
+    // 返回最大价值
+    return K[n][MAX_WEIGHT];
 }
 
 int main()
 {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int arr_size = sizeof(arr) / sizeof(arr[0]);
-    printf("Given array is \n");
-    for (int i = 0; i < arr_size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    mergeSort(arr, 0, arr_size - 1);
-    printf("\nSorted array is \n");
-    for (int i = 0; i < arr_size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+    // 初始化物品数组
+    struct Item items[NUM_ITEMS] = {
+        {10, 60},
+        {20, 100},
+        {30, 120},
+        {40, 140},
+        {50, 160},
+        {10, 60},
+        {20, 100},
+        {30, 120},
+        {40, 140},
+        {50, 160}};
+
+    // 计算最大价值并输出结果
+    int max_value = knapsack(items, NUM_ITEMS);
+    printf("Maximum value: %d\n", max_value);
+
     return 0;
 }
