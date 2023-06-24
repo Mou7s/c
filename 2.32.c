d94b44a171b7bb5c96eca9d16c6157aba7a8b460
Mou7s <liuchuge7777777@gmail.com>
2023-06-24 19:09:32 +0800

    #

--- 2.32.c
+++ 2.32.c
@@ -1,4 +1,4 @@
-// 检测补码加法中的溢出
+// 检测补码减法中的溢出
 #include <stdio.h>
 
 int tadd_ok(int x, int y)
@@ -7,6 +7,12 @@ int tadd_ok(int x, int y)
     int neg_over = x < 0 && y < 0 && sum >= 0;
     int pos_over = x >= 0 && y >= 0 && sum < 0;
     return !neg_over && !pos_over;
+}
+
+int tsub_ok(int x, int y)
+{
+    return tadd_ok(x, -y);
+    // 不产生溢出返回1
 }
 
 int main()
@@ -24,13 +30,13 @@ int main()
 
     x = -2147483648; // 最小的负数
     y = -1;
-    if (tadd_ok(x, y))
+    if (tsub_ok(x, y))
     {
-        printf("%d + %d = no overflow\n", x, y);
+        printf("%d - %d = no overflow\n", x, y);
     }
     else
     {
-        printf("%d + %d = overflow\n", x, y);
+        printf("%d - %d = overflow\n", x, y);
     }
 
     return 0;
