c9414cbe6f204734aec7be5a5ed9791905cf3a49
Mou7s <liuchuge7777777@gmail.com>
2023-06-28 15:54:37 +0800

    #

--- test.c
+++ test.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
-#include <limits.h>
 
-int tadd_ok(int x, int y)
+int div16(int x)
 {
-    int sum = x + y;
-    return (sum - x == y) && (sum - y == x);
-}
-int main()
-{
-
-    int x = INT_MAX; // 假设 INT_MAX 是系统中的最大正整数
-    int y = 1;
-    int sum = x + y;
-
-    // 溢出发生，sum 的值变为 INT_MIN
-    // (sum - x) 的结果将是 INT_MIN - INT_MAX，不等于 y
-    // 因此，(sum - x == y) 不成立
-    // 因此，整个表达式 (sum - x == y) && (sum - y == x) 为假
+    int bias = (x >> 31) & 0xF;
+    return (x + bias) >> 4
 }
