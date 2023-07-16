2878f00149edf6286d92ed1dad824f4c37aa671d
mou7s <liuchuge7777777@gmail.com>
2023-07-17 01:49:56 +0800

    #

--- leetcode/704.c
+++ leetcode/704.c
@@ -8,20 +8,39 @@ int search(int *nums, int numsSize, int target)
     while (left <= right)
     {
         int mid = left + (right - left) / 2;
-
         if (nums[mid] == target)
         {
             return mid;
         }
-        else if (nums[mid] < target)
+        else if (target < nums[mid])
         {
-            left = mid + 1;
+            right = mid - 1;
         }
         else
         {
-            right = mid - 1;
+            left = mid + 1;
         }
     }
 
     return -1;
+}
+
+int main()
+{
+    int nums[] = {1, 2, 3, 4, 5};
+    int target = 3;
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+
+    int result = search(nums, numsSize, target);
+
+    if (result != -1)
+    {
+        printf("Target %d found at index %d\n", target, result);
+    }
+    else
+    {
+        printf("Target %d not found\n", target);
+    }
+
+    return 0;
 }
