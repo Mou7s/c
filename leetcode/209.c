2e29c8cab8496835487b0c216da596cd3c425a44
Mou7s <liuchuge7777777@gmail.com>
2023-07-18 00:04:26 +0800

    #

--- leetcode/209.c
+++ leetcode/209.c
@@ -2,28 +2,40 @@
 #include <limits.h>
 #include <stdlib.h>
 
-int compare(const void *a, const void *b)
-{
-    return *(int *)b - *(int *)a; // 降序排序
-}
-
 int minSubArrayLen(int target, int *nums, int numsSize)
 {
-    qsort(nums, numsSize, sizeof(int), compare); // 对数组进行降序排序
-
     int sum = 0;
     int length = 0;
+    int minLength = INT_MAX;
 
     for (int i = 0; i < numsSize; i++)
     {
         sum += nums[i];
         length++;
-
-        if (sum >= target)
+        while (sum >= target)
         {
-            return length;
+            minLength = (length < minLength) ? length : minLength;
+            sum -= nums[i - length + 1];
+            length--;
         }
     }
 
-    return 0; // 没有找到满足条件的子数组
+    // int sum = 0;
+    // int length = 0;
+    // int minLength = INT_MAX; // 初始化为最大值
+
+    // for (int i = 0; i < numsSize; i++)
+    // {
+    //     sum += nums[i];
+    //     length++;
+
+    //     while (sum >= target) // 使用循环来收缩窗口
+    //     {
+    //         minLength = (length < minLength) ? length : minLength;
+    //         sum -= nums[i - length + 1];
+    //         length--;
+    //     }
+    // }
+
+    // return (minLength != INT_MAX) ? minLength : 0; // 如果找到了满足条件的子数组，返回最小长度，否则返回0
 }
