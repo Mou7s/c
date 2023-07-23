919f4a314b5b6f089330faf58ff554f55f28092d
Mou7s <liuchuge7777777@gmail.com>
2023-07-24 06:59:30 +0800

    #

--- test.c
+++ test.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-#include <math.h>
-
-#define PI 3.1415926
 
 int main()
 {
-    double radius; // 球的半径
-    while (scanf("%lf", &radius) == 1)
+    int n;
+
+    while (scanf("%d", &n) == 1)
     {
-        double volume = (4.0 / 3.0) * PI * pow(radius, 3); // 计算球的体积
-        printf("%.3lf\n", volume);
+        int hundreds = n / 100;    // 百位数
+        int tens = (n % 100) / 10; // 十位数
+        int ones = n % 10;         // 个位数
+
+        printf("%d%d%d\n", ones, tens, hundreds);
     }
+
     return 0;
 }
