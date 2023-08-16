ddb78deb880e6daff26ea0009c99df4382c0827d
mou7s <liuchuge7777777@gmail.com>
2023-08-16 19:25:42 +0800

    #

--- test.c
+++ test.c
@@ -1,8 +1,11 @@
+#include <conio.h>
 #include <stdio.h>
+
 int main()
 {
-    char str1[] = "Hello, world!"; // 字符数组形式
-    char *str2 = "Hello, world!";  // 指针形式
-    printf("%s\n%s\n", str1, str2);
+    char ch;
+    printf("Enter a character: ");
+    ch = getch(); // 获取字符（不显示在屏幕上）
+    printf("\nYou entered: %c\n", ch);
     return 0;
 }
