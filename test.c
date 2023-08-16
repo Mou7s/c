bc0339c287c81808839abbb7b05fe7c362906468
mou7s <liuchuge7777777@gmail.com>
2023-08-16 17:25:23 +0800

    #

--- test.c
+++ test.c
@@ -1,1 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
+int main()
+{
+    char str1[] = "Hello, world!"; // 字符数组形式
+    char *str2 = "Hello, world!";  // 指针形式
+    printf("%s\n%s\n", str1, str2);
+    return 0;
+}
