1dd9dac88592b104e330e227747ae1b2335c6d7d
mou7s <liuchuge7777777@gmail.com>
2023-08-17 19:23:50 +0800

    了。

--- test.c
+++ test.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                // 交换相邻的元素
+                arr[j] = arr[j] ^ arr[j + 1];
+                arr[j + 1] = arr[j] ^ arr[j + 1];
+                arr[j] = arr[j] ^ arr[j + 1];
+            }
+        }
+    }
+}
 int main()
 {
-    char str[30];
-    char c;
-    int i;
-    for (c = 65, i = 0; c <= 90; c++, i++)
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("未排序的数组：\n");
+    for (int i = 0; i < n; i++)
     {
-        str[i] = c;
+        printf("%d ", arr[i]);
     }
-    printf("%s\n", str);
+    printf("\n");
+
+    bubbleSort(arr, n);
+
+    printf("排序后的数组：\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
