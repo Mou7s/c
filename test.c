c965ca7b35825bf6c1c0728bd36fc733899ee2df
mou7s <79881792+Mou7s@users.noreply.github.com>
2023-08-18 13:23:34 +0000

    #

--- test.c
+++ test.c
@@ -1,28 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "./compare.c"
-
+#include <string.h>
 int main()
 {
-
-    int arr[] = {12, 11, 13, 5, 6, 42};
-    int len = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Original array: ");
-    for (int i = 0; i < len; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    qsort(arr, len, sizeof(int), compare);
-
-    printf("Sorted array: ");
-    for (int i = 0; i < len; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
+    char str1[] = "http://c.biancheng.net";
+    char str2[] = "http://www.baidu.com";
+    // 比较两个字符串大小
+    int result = strcmp(str1, str2);
+    printf("str1  - str2 = %d\n", result);
     return 0;
 }
