3f53221b5718c09d8e94611a3740e00e0c12ea59
mou7s <79881792+Mou7s@users.noreply.github.com>
2023-08-18 16:36:16 +0000

    #

--- test.c
+++ test.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
-// 将比较字符串大小的代码封装成函数，并命名为strcmp_alias
-int strcmp_alias(char *s1, char *s2)
+int sum(int m, int n)
 {
-    int i, result;
-    for (i = 0; (result = s1[i] - s2[i]) == 0; i++)
+    int i, sum = 0;
+    for (i = m; i <= n; i++)
     {
-        if (s1[i] == '\0' || s2[i] == '\0')
-        {
-            break;
-        }
+        sum += i;
     }
-
-    return result;
+    return sum;
 }
 int main()
 {
-    char str1[] = "http://c.biancheng.net";
-    char str2[] = "http://www.baidu.com";
-    char str3[] = "http://data.biancheng.net";
-    // 重复使用strcmp_alias()函数
-    int result_1_2 = strcmp_alias(str1, str2);
-    int result_1_3 = strcmp_alias(str1, str3);
-    printf("str1  - str2 = %d\n", result_1_2);
-    printf("str1  - str3 = %d\n", result_1_3);
+    int begin = 5, end = 86;
+    int result = sum(begin, end);
+    printf("The sum from %d to %d is %d\n", begin, end, result);
     return 0;
 }
