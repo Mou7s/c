3461a1b548265b7e3f44e945f0dcc71d418f5cea
mou7s <liuchuge7777777@gmail.com>
2023-08-20 03:29:54 +0800

    #

--- test.c
+++ test.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
-#include <ctype.h>
+// 不同的平台下引入不同的头文件
+#if _WIN32 // 识别windows平台
+#include <windows.h>
+#elif __linux__ // 识别linux平台
+#include <unistd.h>
+#endif
 
 int main()
 {
-    char ch = 'A';
+// 不同的平台下调用不同的函数
+#if _WIN32 // 识别windows平台
 
-    if (isalpha(ch))
-    {
-        printf("%c is an alphabet.\n", ch);
-    }
-    else
-    {
-        printf("%c is not an alphabet.\n", ch);
-    }
-
-    if (isdigit(ch))
-    {
-        printf("%c is a digit.\n", ch);
-    }
-    else
-    {
-        printf("%c is not a digit.\n", ch);
-    }
-
+    printf("windows\n");
+#elif __linux__ // 识别linux平台
+    printf("linux\n");
+#endif
     return 0;
 }
