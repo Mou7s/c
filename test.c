e2dfd3e154980720122f90659a2b98a7401cdfb4
mou7s <liuchuge7777777@gmail.com>
2023-08-19 20:00:58 +0800

    #

--- test.c
+++ test.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
-// 计算从m加到n的值
-int sum(int m, int n)
-{
-    int i;
-    for (i = m + 1; i <= n; ++i)
-    {
-        m += i;
-    }
-    return m;
-}
+#include "./sortingAlgorithm/compare.h"
+
 int main()
 {
-    int a, b, total;
-    printf("Input two numbers: ");
-    scanf("%d %d", &a, &b);
-    total = sum(a, b);
-    printf("a=%d, b=%d\n", a, b);
-    printf("total=%d\n", total);
+    int array[] = {9, 4, 7, 2, 1, 8, 5, 3, 6};   // 待排序的数组
+    int size = sizeof(array) / sizeof(array[0]); // 数组大小
+
+    printf("Original array:\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
+    // 使用 qsort 函数进行排序
+    qsort(array, size, sizeof(array[0]), compare);
+
+    printf("\nSorted array:\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
     return 0;
 }
