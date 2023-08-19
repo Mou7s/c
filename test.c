d009db936592a4904e1e09b252333bfe40865c2a
mou7s <liuchuge7777777@gmail.com>
2023-08-19 20:06:08 +0800

    #

--- test.c
+++ test.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
-#include "./sortingAlgorithm/compare.h"
+
+long long factorial(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1; // 基本情况
+    }
+    else
+    {
+        return factorial(n - 1) * n; // 递归调用
+    }
+}
 
 int main()
 {
-    int array[] = {9, 4, 7, 2, 1, 8, 5, 3, 6};   // 待排序的数组
-    int size = sizeof(array) / sizeof(array[0]); // 数组大小
-
-    printf("Original array:\n");
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", array[i]);
-    }
-
-    // 使用 qsort 函数进行排序
-    qsort(array, size, sizeof(array[0]), compare);
-
-    printf("\nSorted array:\n");
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", array[i]);
-    }
-
+    int num;
+    printf("input number:");
+    scanf("%d", &num);
+    long long result = factorial(num);
+    printf("%lld", result);
     return 0;
 }
