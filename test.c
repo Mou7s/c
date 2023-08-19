e664adad6762e96ecd778a3d98257830d7595ec6
mou7s <liuchuge7777777@gmail.com>
2023-08-19 15:55:41 +0800

    #

--- test.c
+++ test.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
-
-// 函数定义，有两个形参
-int add(int a, int b)
+// 计算从m加到n的值
+int sum(int m, int n)
 {
-    return a + b;
+    int i;
+    for (i = m + 1; i <= n; ++i)
+    {
+        m += i;
+    }
+    return m;
 }
-
 int main()
 {
-    int num1, num2;
-
-    printf("Enter first number: ");
-    scanf("%d", &num1);
-
-    printf("Enter second number: ");
-    scanf("%d", &num2);
-
-    // 函数调用，传递num1和num2作为实参
-    int result = add(num1, num2);
-
-    printf("Sum: %d\n", result);
-
+    int a, b, total;
+    printf("Input two numbers: ");
+    scanf("%d %d", &a, &b);
+    total = sum(a, b);
+    printf("a=%d, b=%d\n", a, b);
+    printf("total=%d\n", total);
     return 0;
 }
