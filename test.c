5a2c7d42a4c0fa153aeca08c363ed38564f4ea4b
mou7s <liuchuge7777777@gmail.com>
2023-08-21 00:50:30 +0800

    pointer9.7

--- test.c
+++ test.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-
+void swap(int *p1, int *p2)
+{
+    int temp; // 临时变量
+    temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
 int main()
 {
-    char c = 'K'; // 定义字符变量 c，存储字符 'K'
-    char *p = &c; // 定义指针变量 p，将其指向变量 c 的地址
-
-    printf("Value of c: %c\n", c);          // 输出变量 c 的值
-    printf("Address of c: %p\n", &c);       // 输出变量 c 的地址
-    printf("Value pointed by p: %c\n", *p); // 通过指针 p 访问变量 c 的值
-    printf("Address stored in p: %p\n", p); // 输出指针 p 存储的地址
-
-    *p = 'Z'; // 通过指针 p 修改变量 c 的值为字符 'Z'
-
-    printf("New value of c: %c\n", c); // 输出修改后的变量 c 的值
-
+    int a = 66, b = 99;
+    swap(&a, &b);
+    printf("a = %d, b = %d\n", a, b);
     return 0;
 }
