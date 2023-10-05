f496390e7600e31d4bc5d6ccbf9af7d8b9b73a51
mou7s <liuchuge7777777@gmail.com>
2023-10-05 22:03:34 +0800

    #

--- algo/bubbleSort.c
+++ algo/bubbleSort.c
@@ -3,15 +3,12 @@
 // 冒泡排序函数
 void bubbleSort(int arr[], int n)
 {
-    int i, j;
-    for (i = 0; i < n - 1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        // 每轮比较相邻的两个元素，将较大的元素往后冒泡
-        for (j = 0; j < n - i - 1; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
-                // 交换 arr[j] 和 arr[j+1] 的位置
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
--- algo/insertionSort.c
+++ algo/insertionSort.c
@@ -3,20 +3,17 @@
 // 插入排序函数
 void insertionSort(int nums[], int size)
 {
-    // 外层循环从第二个元素开始，因为第一个元素默认已经是有序的
     for (int i = 1; i < size; i++)
     {
-        int currentElement = nums[i]; // 保存当前需要插入的元素
-        int j = i - 1;                // j用于在已排序部分查找合适的位置
+        int j = i - 1;
+        int currentElement = nums[i];
 
-        // 在已排序部分寻找合适的插入位置
         while (j >= 0 && nums[j] > currentElement)
         {
-            nums[j + 1] = nums[j]; // 将大于currentElement的元素后移一位
+            nums[j + 1] = nums[j];
             j--;
         }
 
-        // 将currentElement插入到合适的位置
         nums[j + 1] = currentElement;
     }
 }
