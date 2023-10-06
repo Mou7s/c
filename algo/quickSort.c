abc61d56b4590ba6de584b39d67f0e1516a3c4df
mou7s <liuchuge7777777@gmail.com>
2023-10-06 18:35:20 +0800

    #

--- algo/quickSort.c
+++ algo/quickSort.c
@@ -7,53 +7,55 @@ void swap(int *a, int *b)
     *b = temp;
 }
 
-int partition(int arr[], int low, int high)
+void quicksort(int *array, int low, int high)
 {
-    int pivot = arr[high];
-    int i = (low - 1);
-
-    for (int j = low; j <= high - 1; j++)
+    if (low >= high)
     {
-        if (arr[j] < pivot)
+        return;
+    }
+
+    int pivot = array[low];
+    int i = low + 1;
+    int j = high;
+
+    while (i <= j)
+    {
+        while (array[i] < pivot)
         {
             i++;
-            swap(&arr[i], &arr[j]);
+        }
+
+        while (array[j] > pivot)
+        {
+            j--;
+        }
+
+        if (i <= j)
+        {
+            swap(&array[i], &array[j]);
+            i++;
+            j--;
         }
     }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
 
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-        int pi = partition(arr, low, high);
-
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
+    quicksort(array, low, j);
+    quicksort(array, i, high);
 }
 
 int main()
 {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int array[] = {5, 2, 4, 7, 1, 3, 6};
+    int n = sizeof(array) / sizeof(array[0]);
 
-    printf("原始数组：\n");
+    quicksort(array, 0, n - 1);
+
     for (int i = 0; i < n; i++)
     {
-        printf("%d ", arr[i]);
+        printf("%d ", array[i]);
     }
+
     printf("\n");
 
-    quickSort(arr, 0, n - 1);
-
-    printf("排序后的数组：\n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
     return 0;
 }
+s
--- test.c
+++ test.c
@@ -12,7 +12,7 @@ int partition(int arr[], int low, int high)
     int pivot = arr[high];
     int i = (low - 1);
 
-    for (int j = 0; j <= high - 1; j++)
+    for (int j = low; j <= high - 1; j++)
     {
         if (arr[j] < pivot)
         {
