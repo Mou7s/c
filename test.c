147579eea20aef4cc4c76cde605aaac5143aebf8
mou7s <liuchuge7777777@gmail.com>
2023-10-06 17:21:06 +0800

    #

--- test.c
+++ test.c
@@ -1,45 +1,59 @@
 #include <stdio.h>
 
-/* 选择排序 */
-
-void selectSort(int nums[], int size)
+void swap(int *a, int *b)
 {
-    for (int i = 0; i < size - 1; i++)
-    {
-        int min = i;
-        for (int j = i + 1; j < size; j++)
-        {
-            if (nums[j] < nums[min])
-            {
-                min = j;
-            }
-        }
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-        // 交换最小值和当前位置的元素
-        int temp = nums[i];
-        nums[i] = nums[min];
-        nums[min] = temp;
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = 0; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
 }
 
 int main()
 {
-    int arr[] = {12, 11, 13, 5, 6};
+    int arr[] = {64, 25, 12, 22, 11};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    printf("未排序数组：\n");
+    printf("原始数组：\n");
     for (int i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
+    printf("\n");
 
-    selectSort(arr, n);
+    quickSort(arr, 0, n - 1);
 
-    printf("\n已排序数组：\n");
+    printf("排序后的数组：\n");
     for (int i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
-
+    printf("\n");
     return 0;
 }
