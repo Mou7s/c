b74581a484d1018cccd8234d578b0778437cce16
mou7s <liuchuge7777777@gmail.com>
2023-10-07 15:42:41 +0800

    #

--- test.c
+++ test.c
@@ -1,62 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// 定义链表节点的结构
+// 链表节点的结构定义
 struct Node
 {
     int data;
     struct Node *next;
 };
 
-// 初始化一个空链表
-struct Node *head = NULL;
-// 插入一个新节点到链表的末尾
-void insert(int data)
+// 创建一个新节点
+struct Node *createNode(int data)
 {
-    // 创建一个新节点
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    if (newNode == NULL)
+    {
+        printf("内存分配失败\n");
+        exit(1);
+    }
     newNode->data = data;
     newNode->next = NULL;
-
-    // 如果链表为空，将新节点设为头节点
-    if (head == NULL)
-    {
-        head = newNode;
-        return;
-    }
-
-    // 找到链表的最后一个节点
-    struct Node *current = head;
-    while (current->next != NULL)
-    {
-        current = current->next;
-    }
-
-    // 将新节点连接到最后一个节点的后面
-    current->next = newNode;
+    return newNode;
 }
 
-// 打印链表的所有元素
-void printList()
+// 在链表末尾添加节点
+void append(struct Node **head, int data)
+{
+    struct Node *newNode = createNode(data);
+    if (*head == NULL)
+    {
+        *head = newNode;
+    }
+    else
+    {
+        struct Node *current = *head;
+        while (current->next != NULL)
+        {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+// 打印链表的内容
+void printList(struct Node *head)
 {
     struct Node *current = head;
-    printf("链表元素: ");
     while (current != NULL)
     {
-        printf("%d -> ", current->data);
+        printf("%d ", current->data);
         current = current->next;
     }
-    printf("NULL\n");
+    printf("\n");
 }
+
+// 主函数
 int main()
 {
-    // 插入一些元素到链表
-    insert(1);
-    insert(2);
-    insert(3);
+    struct Node *head = NULL;
+
+    // 向链表中添加元素
+    append(&head, 1);
+    append(&head, 2);
+    append(&head, 3);
 
     // 打印链表
-    printList();
+    printf("链表内容：\n");
+    printList(head);
+
+    // 释放链表内存
+    while (head != NULL)
+    {
+        struct Node *temp = head;
+        head = head->next;
+        free(temp);
+    }
 
     return 0;
 }
