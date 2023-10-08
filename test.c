1dba6ac8b2fd406b0feb2da7787a30f6caa67a0f
mou7s <liuchuge7777777@gmail.com>
2023-10-08 11:50:06 +0800

    #

--- test.c
+++ test.c
@@ -1,30 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 100
-
-struct Stack
+struct Node
 {
-    int data[MAX_SIZE];
-    int top;
+    int data;
+    struct Node *next;
 };
 
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
 
+void append(struct Node **head, int data)
+{
+    struct Node *newNode = createNode(data);
+    if (*head == NULL)
+    {
+        *head == *newNode;
+    }
+    else
+    {
+        struct Node *current = *head;
+    }
+}
 
+// 主函数
 int main()
 {
-    struct Stack stack;
-    initStack(&stack);
+    struct Node *head = NULL;
 
-    push(&stack, 1);
-    push(&stack, 2);
-    push(&stack, 3);
+    // 向链表中添加元素
+    append(&head, 1);
+    append(&head, 2);
+    append(&head, 3);
 
-    printf("栈顶元素：%d\n", peek(&stack));
+    // 打印链表
+    printf("链表内容：\n");
+    printList(head);
 
-    while (!isEmpty(&stack))
+    // 释放链表内存
+    while (head != NULL)
     {
-        printf("出栈元素：%d\n", pop(&stack));
+        struct Node *temp = head;
+        head = head->next;
+        free(temp);
     }
 
     return 0;
