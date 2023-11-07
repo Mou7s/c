7ac8cad07787de91d297f136cb0572d640553bf5
mou7s <liuchuge7777777@gmail.com>
2023-11-07 12:59:57 +0800

    #

--- test.c
+++
@@ -1,4 +0,0 @@
-// 分治法求最大子数组和
-
-#include <stdio.h>
-#include <stdlib.h>
--- test.cpp
+++ test.cpp
@@ -1,6 +1,15 @@
-// 命令模式
-
-#include <iostream>
 #include <vector>
+#include <iostream>
 
-using namespace std;
+int sum(std::vector<int> nums)
+{
+    if (nums.size() <= 0)
+    {
+        return 0;
+    }
+    else
+    {
+        nums.erase(nums.begin());
+        return nums[0] + sum(nums);
+    }
+}
