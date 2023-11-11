9b32b0706c7150380315e0195206f25b60f19dc7
mou7s <liuchuge7777777@gmail.com>
2023-11-11 18:44:26 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,4 +1,6 @@
-int opposite(int number) 
+#include <string>
+
+std::string createPhoneNumber(const int arr[10])
 {
-    return -number;
+    return std::string("(") + std::to_string(arr[0]) + std::to_string(arr[1]) + std::to_string(arr[2]) + std::string(") ") + std::to_string(arr[3]) + std::to_string(arr[4]) + std::to_string(arr[5]) + std::string("-") + std::to_string(arr[6]) + std::to_string(arr[7]) + std::to_string(arr[8]) + std::to_string(arr[9]);
 }
