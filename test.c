df310b9694b349f8200cf7540c3ff1a55c3922ba
mou7s <liuchuge7777777@gmail.com>
2024-07-08 15:57:24 +0800

    #

--- test.c
+++ test.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int sumOfElement(int a[], int size)
+{
+    int i, sum = 0;
+    for (i = 0; i < size; i++)
+    {
+        sum += a[i];
+    }
+    return sum;
+}
 int main()
 {
-    printf("Hello, World!\n");
-    return 0;
+    int a[] = {1, 2, 3, 4, 5};
+    int total = sumOfElement(a, sizeof(a) / sizeof(a[0]));
+    printf("Sum of elements: %d\n", total);
 }
