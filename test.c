a9dba483d4c1db4a4f8adfae49d46ea833c68cf7
mou7s <liuchuge7777777@gmail.com>
2024-07-09 22:58:21 +0800

    chore: Add support for variable arguments in test.c

--- test.c
+++ test.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int sumOfElement(int a[], int size)
-{
-    int i, sum = 0;
-    for (i = 0; i < size; i++)
-    {
-        sum += a[i];
-    }
-    return sum;
-}
-int main()
-{
-    int a[] = {1, 2, 3, 4, 5};
-    int total = sumOfElement(a, sizeof(a) / sizeof(a[0]));
-    printf("Sum of elements: %d\n", total);
-}
+可变参数
