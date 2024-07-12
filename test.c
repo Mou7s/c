78f975cd6d7f8ef29a72f6056ab2f4f8b35d7df3
mou7s <liuchuge7777777@gmail.com>
2024-07-12 17:31:52 +0800

    feat: Add strcpy function to test.c

--- test.c
+++ test.c
@@ -1,1 +1,18 @@
-可变参数
+#include <stdio.h>
+#include <string.h>
+
+char *strcpy(char *dest, const char *source)
+{
+    char *ptr = dest;
+    while (*dest++ = *source++)
+        ;
+    return ptr;
+}
+
+int main(void)
+{
+    char str[25];
+    strcpy(str, "hello world");
+    printf("%s\n", str);
+    return 0;
+}
