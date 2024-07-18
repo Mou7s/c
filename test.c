820667f4662c4e6b9a2b8632d90befd73d4c4b57
mou7s <liuchuge7777777@gmail.com>
2024-07-18 13:56:33 +0800

    Refactor test.c to use scanf for input and improve date formatting

--- test.c
+++ test.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
-#include <time.h>
-
-#define __DATE__ "July 23 2021"
-#define __TIME__ "16:45:00"
-#define __FILE__ "test.c"
-#define __LINE__ 4
-#define __STDC__ 1
 
 int main()
 {
-    printf("Date: %s\n", __DATE__);
-    printf("Time: %s\n", __TIME__);
-    printf("File: %s\n", __FILE__);
-    printf("Line: %d\n", __LINE__);
-    printf("STDC: %d\n", __STDC__);
-    return 0;
+    int year, month, day;
+    scanf("%d%*c%d%*c%d", &year, &month, &day);
+    printf("%d-%02d-%02d\n", year, month, day);
 }
