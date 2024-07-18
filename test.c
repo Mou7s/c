d48179f7d9140beb2ff0bcdaccc989a654a56a8a
mou7s <liuchuge7777777@gmail.com>
2024-07-18 16:15:37 +0800

    Update test.c

--- test.c
+++ test.c
@@ -3,6 +3,20 @@
 
 int main(void)
 {
-    
+    FILE *file = fopen("test.txt", "r");
+    if (file == NULL)
+    {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
+
+    int c;
+    while ((c = fgetc(file)) != EOF)
+    {
+        putchar(c);
+    }
+
+    fclose(file); // 完成操作后关闭文件
+
     return 0;
 }
