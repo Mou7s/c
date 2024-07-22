9de59bc7e4462ebfcff385a179a6b38ec8f631f0
mou7s <liuchuge7777777@gmail.com>
2024-07-22 17:32:03 +0800

    Create 3.11.4.c

---
+++ cPrimerPlus/3.11.4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    // enter a floating-point value
+    float f;
+    printf("Enter a floating-point value: ");
+    scanf("%f", &f);
+
+    // fixed-point  notation
+    
+}
